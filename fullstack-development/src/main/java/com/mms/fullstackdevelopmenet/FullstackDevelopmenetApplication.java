055279517dba19a3fc1cc7f65a334fc7c30d643d
SwatiDhanade811 <96002384+SwatiDhanade811@users.noreply.github.com>
2023-09-24 17:37:21 +0530

    Add files via upload

---
+++ fullstack-development/src/main/java/com/mms/fullstackdevelopmenet/FullstackDevelopmenetApplication.java
@@ -0,0 +1,13 @@
+package com.mms.fullstackdevelopmenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackDevelopmenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackDevelopmenetApplication.class, args);
+	}
+
+}
---
+++ fullstack-development/src/test/java/com/mms/fullstackdevelopmenet/FullstackDevelopmenetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mms.fullstackdevelopmenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackDevelopmenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
